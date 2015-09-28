18e69f4ecd08fd103388a39027ab8dc88849b63c
Emiliano Mongelo <emiliano@fanbridge.com>
2015-09-28 14:21:23 -0300

    Reply de reply

--- Overcoat/server/resources/mocks/replies.js
+++ Overcoat/server/resources/mocks/replies.js
@@ -21,6 +21,31 @@ exports.replies = [
     downvotes: 294,
     tips: 6,
     shares: 19,
-    picture: undefined
+    picture: undefined,
+    replies: [{
+        id: 2,
+        userId: 1,
+        coatId: 3,
+        replyId: 2,
+        siteId: 1,
+        message: "Reply de reply",
+        upvotes: 99,
+        downvotes: 245,
+        tips: 9,
+        shares: 39,
+        picture: undefined,
+        user: {
+            avatar: "/statics/images/avatar-01.png",
+            coats: 123,
+            email: "mayraskinner@kozgene.com",
+            followers: 123,
+            following: 432,
+            id: "1",
+            name: "Shauna Vaughan",
+            registered: "2014-08-31T04:15:59 +03:00",
+            screenName: "Mayra.Skinner",
+            upvotes: 84
+        }
+    }]
   }
 ];
