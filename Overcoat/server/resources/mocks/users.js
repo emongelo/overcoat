f1d1ba7d180c9af586fad17745c0e26a46f10966
Emiliano Mongelo <emiliano@fanbridge.com>
2015-10-03 16:08:14 -0300

    Temp

--- Overcoat/server/resources/mocks/users.js
+++ Overcoat/server/resources/mocks/users.js
@@ -1,40 +1,40 @@
 exports.users = [
-  {
-    "id": "1",
-    "avatar": "/statics/images/avatar-01.png",
-    "name": "Shauna Vaughan",
-    "screenName": "Mayra.Skinner",
-    "email": "mayraskinner@kozgene.com",
-    "registered": "2014-08-31T04:15:59 +03:00",
-    "followers": 123,
-    "coats": 123,
-    "following": 432,
-    "upvotes": 84
-  },
-  {
-    "id": "2",
-    "avatar": "/statics/images/avatar-02.png",
-    "name": "Hebert Mcdowell",
-    "screenName": "Nola.Pruitt",
-    "email": "nolapruitt@kozgene.com",
-    "registered": "2015-05-16T18:05:37 +03:00",
-    "followers": 123,
-    "coats": 123,
-    "following": 432,
-    "upvotes": 84
-  },
-  {
-    "id": "3",
-    "avatar": "/statics/images/avatar-03.png",
-    "name": "Nancy Sloan",
-    "screenName": "Scott.Cooper",
-    "email": "scottcooper@kozgene.com",
-    "registered": "2015-04-26T18:35:23 +03:00",
-    "followers": 123,
-    "coats": 123,
-    "following": 432,
-    "upvotes": 84
-  },
+	{
+		"id": "1",
+		"avatar": "/statics/images/avatar-01.png",
+		"name": "Shauna Vaughan",
+		"screenName": "Mayra.Skinner",
+		"email": "mayraskinner@kozgene.com",
+		"registered": "2014-08-31T04:15:59 +03:00",
+		"followers": 425,
+		"coats": 236,
+		"following": 136,
+		"upvotes": 67
+	},
+	{
+		"id": "2",
+		"avatar": "/statics/images/avatar-02.png",
+		"name": "Hebert Mcdowell",
+		"screenName": "Nola.Pruitt",
+		"email": "nolapruitt@kozgene.com",
+		"registered": "2015-05-16T18:05:37 +03:00",
+		"followers": 672,
+		"coats": 347,
+		"following": 127,
+		"upvotes": 72
+	},
+	{
+		"id": "3",
+		"avatar": "/statics/images/avatar-03.png",
+		"name": "Nancy Sloan",
+		"screenName": "Scott.Cooper",
+		"email": "scottcooper@kozgene.com",
+		"registered": "2015-04-26T18:35:23 +03:00",
+		"followers": 723,
+		"coats": 774,
+		"following": 813,
+		"upvotes": 185
+	},
   {
     "id": "4",
     "avatar": "/statics/images/avatar-04.png",
