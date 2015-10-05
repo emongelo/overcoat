490be2c4af6dd77c716f529065260f7b03abf322
Emiliano Mongelo <emiliano@fanbridge.com>
2015-10-05 00:03:52 -0300

    New file

---
+++ Overcoat/server/resources/mocks/invitations.js
@@ -0,0 +1,17 @@
+exports.invitations = [
+  {
+	  userID: 89423,
+    name: "Ernesto Sábato",
+    timestamp: '2015-04-26T18:35:23 +03:00'
+  },
+	{
+		userID: 433243,
+		name: "Greg Deamon",
+		timestamp: '2015-04-26T18:35:23 +03:00'
+	},
+	{
+		userID: 43223,
+		name: "Will Smith",
+		timestamp: '2015-04-26T18:35:23 +03:00'
+	}
+];
